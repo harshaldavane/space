46093ddf57db15c4af6337719e6f53e5a70e1b5b
harshaldavane <sagardavane@gmail.com>
2023-05-25 15:38:11 +0530

    navbar almost done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DestinationComponent } from './destination/destination.component';
+import { CrewComponent } from './crew/crew.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'destination',
+    component: DestinationComponent
+  },
+  {
+    path: 'crew',
+    component: CrewComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
