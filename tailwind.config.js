a83927682c1c2b1cb96016f283cd3d2781ef1c19
harshaldavane <sagardavane@gmail.com>
2023-05-30 13:21:18 +0530

    home page ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '200px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      'Bellefair':['Bellefair', 'sans-serif'],
+      'BarlowCondensed':['Barlow Condensed', 'sans-serif'],
+      'Barlow':['Barlow', 'sans-serif']
+    }
   },
   plugins: [],
 }
