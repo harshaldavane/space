b29694599a39adea4412d139a8f01bff160c2d7b
harshaldavane <sagardavane@gmail.com>
2023-06-07 15:32:34 +0530

    json file aaded

--- src/app/destination/destination.component.ts
+++ src/app/destination/destination.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import DataJson from '../../assets/data.json';
 
 @Component({
   selector: 'app-destination',
   templateUrl: './destination.component.html',
   styleUrls: ['./destination.component.scss']
 })
+
+
+
 export class DestinationComponent {
 
-  text:string="Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!"
+  countriesList:any[] = DataJson;
 
+  text:String="";
   moon(){
+   this.text="Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!";
   }
 }
+
