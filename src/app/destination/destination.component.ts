e80e507bf456da15ae83983bc265171745b170c2
harshaldavane <sagardavane@gmail.com>
2023-06-09 18:00:41 +0530

    added fields in ts file

--- src/app/destination/destination.component.ts
+++ src/app/destination/destination.component.ts
@@ -11,21 +11,22 @@ import DataJson from '../../assets/data.json';
 
 export class DestinationComponent {
     arrayData: any = [];
-    headerVal:String =this.arrayData.
-    moonImgAdd:String = ""
-    moonDescription = ""
-    distance:String = ""
+    headerVal:String =this.arrayData.destinations[0].name
+    moonImgAdd:String = this.arrayData.destinations[0].images.png
+    moonDescription:String = this.arrayData.destinations[0].description
+    distance:String=this.arrayData.destinations[0].distance
+    travel:String=this.arrayData.destinations[0].travel
     constructor(){
     this.arrayData = DataJson;
     console.log(this.arrayData.destinations[0].name);
-
   }
 
   mars(){
    this.headerVal = this.arrayData.destinations[1].name
    this.moonImgAdd = this.arrayData.destinations[1].images.png
-   this.moonDescription = this.arrayData.description
-   this.distance = ""
+   this.moonDescription = this.arrayData.destinations[1].description
+   this.distance=this.arrayData.destinations[1].distance
+   this.travel=this.arrayData.destinations[1].travel
   }
 
 
