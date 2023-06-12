7552392f4cf5a2d011951903c88dd9330ef9b697
harshaldavane <sagardavane@gmail.com>
2023-06-12 12:25:02 +0530

    destination page done

--- src/app/destination/destination.component.ts
+++ src/app/destination/destination.component.ts
@@ -11,15 +11,28 @@ import DataJson from '../../assets/data.json';
 
 export class DestinationComponent {
     arrayData: any = [];
-    headerVal:String =this.arrayData.destinations[0].name
-    moonImgAdd:String = this.arrayData.destinations[0].images.png
-    moonDescription:String = this.arrayData.destinations[0].description
-    distance:String=this.arrayData.destinations[0].distance
-    travel:String=this.arrayData.destinations[0].travel
+    moonImgAdd:any=""
+    headerVal:any=""
+    moonDescription:any=""
+    distance:any=""
+    travel:any=""
     constructor(){
     this.arrayData = DataJson;
-    console.log(this.arrayData.destinations[0].name);
+    console.log(this.arrayData.destinations[0]);
+    this.moon();
   }
+
+  moon(){
+    this.moonImgAdd = this.arrayData.destinations[0].images.png;
+
+    this.headerVal =this.arrayData.destinations[0].name;
+
+    this.moonDescription = this.arrayData.destinations[0].description;
+
+    this.distance = this.arrayData.destinations[0].distance;
+
+    this.travel=this.arrayData.destinations[0].travel;
+   }
 
   mars(){
    this.headerVal = this.arrayData.destinations[1].name
@@ -29,5 +42,20 @@ export class DestinationComponent {
    this.travel=this.arrayData.destinations[1].travel
   }
 
+  uropa(){
+    this.headerVal = this.arrayData.destinations[2].name
+    this.moonImgAdd = this.arrayData.destinations[2].images.png
+    this.moonDescription = this.arrayData.destinations[2].description
+    this.distance=this.arrayData.destinations[2].distance
+    this.travel=this.arrayData.destinations[2].travel
+   }
+
+   titan(){
+    this.headerVal = this.arrayData.destinations[3].name
+    this.moonImgAdd = this.arrayData.destinations[3].images.png
+    this.moonDescription = this.arrayData.destinations[3].description
+    this.distance=this.arrayData.destinations[3].distance
+    this.travel=this.arrayData.destinations[3].travel
+   }
 
 }
